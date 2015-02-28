c13667fdd4e8b1b577067a294fc348f403636484
Carlisle Chan <duziyixiangsi@gmail.com>
2015-02-28 22:28:33 +0800

    support set color of StatusBar

--- app/src/main/java/com/carlisle/songtaste/base/BaseActivity.java
+++ app/src/main/java/com/carlisle/songtaste/base/BaseActivity.java
@@ -1,9 +1,13 @@
 package com.carlisle.songtaste.base;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.github.stephanenicolas.loglifecycle.LogLifeCycle;
+import com.readystatesoftware.systembartint.SystemBarTintManager;
 
 /**
  * Created by chengxin on 2/13/15.
@@ -14,10 +18,40 @@ public class BaseActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        initStatusBar();
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
+    }
+
+    protected void initStatusBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window window = getWindow();
+            // Translucent status bar
+            window.setFlags(
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            // Translucent navigation bar
+            window.setFlags(
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION,
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+
+            // create our manager instance after the content view is set
+            SystemBarTintManager tintManager = new SystemBarTintManager(this);
+            // enable status bar tint
+            tintManager.setStatusBarTintEnabled(true);
+            // enable navigation bar tint
+            tintManager.setNavigationBarTintEnabled(true);
+
+            tintManager.setTintColor(setStatusBarColor());
+
+        }
+    }
+
+    protected int setStatusBarColor() {
+        return getResources().getColor(android.R.color.transparent);
     }
 }
