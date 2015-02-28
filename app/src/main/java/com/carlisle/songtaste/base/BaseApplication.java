c7d99db58c7f554d736f5c8e4413574bb5652d26
Carlisle Chan <duziyixiangsi@gmail.com>
2015-03-01 00:17:37 +0800

    modify BaseApplication

--- app/src/main/java/com/carlisle/songtaste/base/BaseApplication.java
+++ app/src/main/java/com/carlisle/songtaste/base/BaseApplication.java
@@ -2,11 +2,17 @@ package com.carlisle.songtaste.base;
 
 import android.app.Application;
 
+import com.carlisle.songtaste.services.AudioPlaybackService;
+
 /**
  * Created by chengxin on 2/13/15.
  */
 public class BaseApplication extends Application {
     private static BaseApplication instance;
+
+    //Service reference and flags.
+    private AudioPlaybackService mService;
+    private boolean mIsServiceRunning = false;
 
     public static BaseApplication getInstance() {
         return instance;
@@ -16,5 +22,21 @@ public class BaseApplication extends Application {
     public void onCreate() {
         super.onCreate();
         instance = this;
+    }
+
+    public boolean isServiceRunning() {
+        return mIsServiceRunning;
+    }
+
+    public void setIsServiceRunning(boolean mIsServiceRunning) {
+        this.mIsServiceRunning = mIsServiceRunning;
+    }
+
+    public AudioPlaybackService getService() {
+        return mService;
+    }
+
+    public void setService(AudioPlaybackService mService) {
+        this.mService = mService;
     }
 }
