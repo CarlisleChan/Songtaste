8664225fa5fdf660d3300746e89abc764b95f761
Carlisle Chan <duziyixiangsi@gmail.com>
2015-03-05 22:26:37 +0800

    add SystemBarTintManager and PagerSlidingTabStrip

--- app/src/main/java/com/carlisle/songtaste/ui/MainActivity.java
+++ app/src/main/java/com/carlisle/songtaste/ui/MainActivity.java
@@ -1,98 +1,76 @@
 package com.carlisle.songtaste.ui;
 
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
-import android.support.v4.widget.DrawerLayout;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.FrameLayout;
 
 import com.carlisle.songtaste.R;
-import com.carlisle.songtaste.base.BaseActivity;
-import com.carlisle.songtaste.modle.FMNewResult;
-import com.carlisle.songtaste.provider.ApiFactory;
-import com.carlisle.songtaste.provider.converter.GsonConverter;
-import com.carlisle.songtaste.ui.drawer.NavigationDrawerFragment;
+import com.carlisle.songtaste.ui.discover.DiscoverFragment;
+import com.readystatesoftware.systembartint.SystemBarTintManager;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
-import rx.Observer;
 
-/**
- * Created by chengxin on 2/13/15.
- */
-public class MainActivity extends BaseActivity {
 
+public class MainActivity extends ActionBarActivity {
+
+    @InjectView(R.id.toolbar)
+    Toolbar toolbar;
     @InjectView(R.id.container)
     FrameLayout container;
-    @InjectView(R.id.drawer)
-    DrawerLayout drawer;
-    @InjectView(R.id.action_bar)
-    Toolbar toolbar;
 
-    private NavigationDrawerFragment drawerFragment;
+    private Drawable oldBackground = null;
+    private SystemBarTintManager mTintManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.inject(this);
-
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        drawerFragment = getmNavigationDrawerFragment();
-        drawerFragment.setup(R.id.fragment_drawer, (DrawerLayout) findViewById(R.id.drawer), toolbar);
+        // create our manager instance after the content view is set
+        mTintManager = new SystemBarTintManager(this);
+        // enable status bar tint
+        mTintManager.setStatusBarTintEnabled(true);
+
+        changeColor(getResources().getColor(android.R.color.holo_green_light));
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.container, new DiscoverFragment());
+        fragmentTransaction.commit();
+
     }
 
-    private NavigationDrawerFragment getmNavigationDrawerFragment() {
-        NavigationDrawerFragment navigationDrawerFragment = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_drawer);
-        if (navigationDrawerFragment == null) {
-            return new NavigationDrawerFragment();
+    private void changeColor(int newColor) {
+        mTintManager.setTintColor(newColor);
+        // change ActionBar color just if an ActionBar is available
+        Drawable colorDrawable = new ColorDrawable(newColor);
+        Drawable bottomDrawable = new ColorDrawable(getResources().getColor(android.R.color.transparent));
+        LayerDrawable ld = new LayerDrawable(new Drawable[]{colorDrawable, bottomDrawable});
+        if (oldBackground == null) {
+            getSupportActionBar().setBackgroundDrawable(ld);
+        } else {
+            TransitionDrawable td = new TransitionDrawable(new Drawable[]{oldBackground, ld});
+            getSupportActionBar().setBackgroundDrawable(td);
+            td.startTransition(200);
         }
-        return navigationDrawerFragment;
+
+        oldBackground = ld;
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-
-
-            ApiFactory.getSongtasteApi(new GsonConverter(GsonConverter.ConverterType.FM_NEW_RESULT)).recList("1", "1", "0", "dm.st.fmNew")
-                    .subscribe(new Observer<FMNewResult>() {
-                        @Override
-                        public void onCompleted() {
-                            Log.d("====>", "onCompleted");
-                        }
-
-                        @Override
-                        public void onError(Throwable e) {
-                            e.printStackTrace();
-                            Log.d("====>", "onError");
-                        }
-
-                        @Override
-                        public void onNext(FMNewResult songListResult) {
-                            Log.d("recList====>", "" + songListResult.code);
-                            Log.d("recList====>", songListResult.data.get(0).Name);
-                        }
-                    });
-
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
 }
--- app/src/main/java/com/carlisle/songtaste/ui/discover/DiscoverFragment.java
+++ app/src/main/java/com/carlisle/songtaste/ui/discover/DiscoverFragment.java
@@ -1,9 +1,71 @@
 package com.carlisle.songtaste.ui.discover;
 
-import com.carlisle.songtaste.base.BaseFragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.astuetz.PagerSlidingTabStrip;
+import com.carlisle.songtaste.R;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
- * Created by chengxin on 2/25/15.
+ * Created by chengxin on 3/5/15.
  */
-public class DiscoverFragment extends BaseFragment {
+public class DiscoverFragment extends Fragment {
+
+    @InjectView(R.id.tabs)
+    PagerSlidingTabStrip tabs;
+    @InjectView(R.id.pager)
+    ViewPager pager;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_discover, container, false);
+        ButterKnife.inject(this, view);
+
+        pager.setAdapter(new DiscoverAdapter(getChildFragmentManager()));
+        tabs.setViewPager(pager);
+        tabs.setBackgroundColor(getResources().getColor(android.R.color.holo_green_light));
+        tabs.setOnTabReselectedListener(new PagerSlidingTabStrip.OnTabReselectedListener() {
+            @Override
+            public void onTabReselected(int i) {
+                Toast.makeText(getActivity(), "" + i, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
+    }
+
+    public class DiscoverAdapter extends FragmentPagerAdapter {
+
+        private final String[] TITLES = {"最新", "热门", "专辑", "标签"};
+
+        public DiscoverAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return TITLES[position];
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return SuperAwesomeCardFragment.newInstance(position);
+        }
+
+        @Override
+        public int getCount() {
+            return TITLES.length;
+        }
+    }
 }
