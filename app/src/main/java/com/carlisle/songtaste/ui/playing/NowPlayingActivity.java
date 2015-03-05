01372e15fdad016a8d85fd6e84edc8ede543066e
Carlisle Chan <duziyixiangsi@gmail.com>
2015-03-05 22:27:15 +0800

    add fake NowPlayingActivity

---
+++ app/src/main/java/com/carlisle/songtaste/ui/playing/NowPlayingActivity.java
@@ -0,0 +1,9 @@
+package com.carlisle.songtaste.ui.playing;
+
+import com.carlisle.songtaste.base.BaseActivity;
+
+/**
+ * Created by carlisle on 3/5/15.
+ */
+public class NowPlayingActivity extends BaseActivity {
+}
