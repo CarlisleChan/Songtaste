6813bc76d4db8f02a9bea4f8072fbcdee0518751
Carlisle Chan <duziyixiangsi@gmail.com>
2015-03-31 00:17:21 +0800

    add SwipeBack

--- app/src/main/java/com/carlisle/songtaste/ui/about/AboutActivity.java
+++ app/src/main/java/com/carlisle/songtaste/ui/about/AboutActivity.java
@@ -3,6 +3,7 @@ package com.carlisle.songtaste.ui.about;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import com.carlisle.songtaste.R;
@@ -10,17 +11,22 @@ import com.carlisle.songtaste.base.BaseActivity;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import me.imid.swipebacklayout.lib.SwipeBackLayout;
+import me.imid.swipebacklayout.lib.app.SwipeBackActivityBase;
+import me.imid.swipebacklayout.lib.app.SwipeBackActivityHelper;
 
 /**
  * Created by carlisle on 3/7/15.
  */
-public class AboutActivity extends BaseActivity {
+public class AboutActivity extends BaseActivity implements SwipeBackActivityBase {
     @InjectView(R.id.tv_about)
     TextView about;
     @InjectView(R.id.tv_app_version)
     TextView appVersion;
     @InjectView(R.id.toolbar)
     Toolbar toolbar;
+
+    private SwipeBackActivityHelper mHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +40,8 @@ public class AboutActivity extends BaseActivity {
         getSupportActionBar().setTitle("关于");
 
         appVersion.setText("anyway.. it's 1.0.0");
+        mHelper = new SwipeBackActivityHelper(this);
+        mHelper.onActivityCreate();
 
     }
 
@@ -43,5 +51,35 @@ public class AboutActivity extends BaseActivity {
             finish();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mHelper.onPostCreate();
+    }
+
+    @Override
+    public View findViewById(int id) {
+        View v = super.findViewById(id);
+        if (v != null)
+            return v;
+        return mHelper.findViewById(id);
+    }
+
+    @Override
+    public SwipeBackLayout getSwipeBackLayout() {
+        return mHelper.getSwipeBackLayout();
+    }
+
+    @Override
+    public void setSwipeBackEnable(boolean enable) {
+        getSwipeBackLayout().setEnableGesture(enable);
+    }
+
+    @Override
+    public void scrollToFinishActivity() {
+        getSwipeBackLayout().scrollToFinishActivity();
     }
 }
--- app/src/main/java/com/carlisle/songtaste/ui/setting/SettingActivity.java
+++ app/src/main/java/com/carlisle/songtaste/ui/setting/SettingActivity.java
@@ -23,11 +23,14 @@ import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
 import de.greenrobot.event.EventBus;
+import me.imid.swipebacklayout.lib.SwipeBackLayout;
+import me.imid.swipebacklayout.lib.app.SwipeBackActivityBase;
+import me.imid.swipebacklayout.lib.app.SwipeBackActivityHelper;
 
 /**
  * Created by carlisle on 3/7/15.
  */
-public class SettingActivity extends BaseActivity {
+public class SettingActivity extends BaseActivity implements SwipeBackActivityBase{
 
     @InjectView(R.id.cb_use_gprs)
     CheckBox useGprsCheckBox;
@@ -38,6 +41,7 @@ public class SettingActivity extends BaseActivity {
 
     private Double time;
     private Handler killSelf;
+    private SwipeBackActivityHelper mHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +53,9 @@ public class SettingActivity extends BaseActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setTitle("设置");
+
+        mHelper = new SwipeBackActivityHelper(this);
+        mHelper.onActivityCreate();
 
     }
 
@@ -145,4 +152,32 @@ public class SettingActivity extends BaseActivity {
 
     }
 
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mHelper.onPostCreate();
+    }
+
+    @Override
+    public View findViewById(int id) {
+        View v = super.findViewById(id);
+        if (v != null)
+            return v;
+        return mHelper.findViewById(id);
+    }
+
+    @Override
+    public SwipeBackLayout getSwipeBackLayout() {
+        return mHelper.getSwipeBackLayout();
+    }
+
+    @Override
+    public void setSwipeBackEnable(boolean enable) {
+        getSwipeBackLayout().setEnableGesture(enable);
+    }
+
+    @Override
+    public void scrollToFinishActivity() {
+        getSwipeBackLayout().scrollToFinishActivity();
+    }
 }
